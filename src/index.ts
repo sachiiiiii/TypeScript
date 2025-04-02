f185cb5da0e306a209ca1595379191eb4030fe01
sachiiiiii3 <osachi752@gmail.com>
2025-04-02 22:20:23 +0000

    completed part 4

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,6 @@ class Vehicle {
     this.make = make;
     this.model = model;
     this.wheels = wheels;
-    // Properties do not exist on type 'Vehicle'
   }
   start(): void {
     this.status = "started";
@@ -56,7 +55,6 @@ const myBuick = new Car("Buick", "Regal");
 myBuick.wheels = myBuick.wheels - 1;
 console.log(myBuick.wheels);
 console.log(myBuick.model);
-// Properties 'wheels' and 'model' do not exist on type 'Car'
 
 /**
  * Part 3: Creating a Generic Class
@@ -120,7 +118,9 @@ class NCycle<T> {
     else if (Array.isArray(this.make) && Array.isArray(this.model)) {
       for (let idx = 0; idx < this.make.length; idx++) {
         if (this.make[idx] === this.make[idx]) {
-          console.log(`This NCycle has a <${this.make}> <${this.model}> at <${idx}>.`);
+          console.log(
+            `This NCycle has a <${this.make}> <${this.model}> at <${idx}>.`
+          );
         }
       }
     }
@@ -130,3 +130,39 @@ class NCycle<T> {
     }
   }
 }
+
+/**
+ * Part 4: Testing
+ */
+
+// Rudimentary testing Code, not part of the assignment
+const testCycle1 = new NCycle<number>(1, 2, 3);
+testCycle1.print();
+testCycle1.printAll();
+
+const testCycle2 = new NCycle<string>("This", "That", 4);
+testCycle2.print();
+testCycle2.printAll();
+
+const testCycle3 = new NCycle<string>("Make", "10", 4);
+testCycle3.print(4);
+testCycle3.printAll();
+
+const makes4 = ["Volkswagon", "Tesla", "Audi"];
+const models4 = ["Passat", "Model X", "A4"];
+const testCycle4 = new NCycle<string[]>(makes4, models4, 4);
+testCycle4.print(2);
+testCycle4.printAll();
+
+const makes5 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const models5 = [1, 1, 2, 3, 5];
+const testCycle5 = new NCycle<number[]>(makes5, models5, 0);
+testCycle5.print(7);
+testCycle5.printAll();
+
+function add(x: number, y: number): number {
+  return x + y;
+}
+add(testCycle1.make, testCycle5.model[1]);
+// Error expected here
+add(testCycle2.make, testCycle4.model[1]);
