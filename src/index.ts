0f88b41dcd87e4a7e47a08fdd38eb401369301d8
sachiiiiii3 <osachi752@gmail.com>
2025-04-02 15:23:07 +0000

    created NCycle class and modified its type and types of class and constructor properties

--- src/index.ts
+++ src/index.ts
@@ -61,17 +61,45 @@ console.log(myBuick.model);
 /**
  * Part 3: Creating a Generic Class
  */
-// class NCycle<T> {
-//   // Create a new method print, which returns nothing and has a single number parameter (either optional or defaulted to 0).
-//   print(num?: number = 0) {
-//     //   Use type guards and other appropriate techniques to implement print such that it logs:
-//     //     "This is a <make> <model> NCycle." if make and model are not arrays.
-//     //     "This NCycle has a <make> <model> at <parameter>." if make and model are arrays and the index of parameter exists in each.
-//     //     "This NCycle was not created properly." if neither of the above are true.
-//   }
 
-//   // Create a new method printAll, which returns nothing and has no parameters.
-//   printAll() {
-//     // Use type guards and appropriate techniques to implement printAll such that it logs the same statements as print, but for all matching pairs in the make and model arrays, if applicable.
-//   }
-// }
+// Modify NCycle to accept a generic type.
+class NCycle<T> {
+  // use a union of literals to declare valid status options
+  // "started" or "stopped"
+  status: "stopped" | "started" = "stopped";
+
+  // Allow make and model to have either the generic type or an array of the generic type.
+  make: T | T[];
+  model: T | T[];
+
+  wheels: number;
+
+  // Adjust the constructor parameters accordingly.
+  constructor(make: T | T[], model: T | T[], wheels: number) {
+    this.make = make;
+    this.model = model;
+    this.wheels = wheels;
+    // Properties do not exist on type 'Vehicle'
+  }
+  start(): void {
+    this.status = "started";
+  }
+  stop(): void {
+    this.status = "stopped";
+  }
+  // Create a new method print, which returns nothing and has a single number parameter (either optional or defaulted to 0).
+  print(num?: number = 0): void {
+    //Use type guards and other appropriate techniques to log:
+    //"This is a <make> <model> NCycle." if make and model are not arrays.
+    if (typeof this.make !== "object" && typeof this.model !== "object") {
+      console.log(`This is a <${this.make}> <${this.model}> NCycle.`);
+    }
+    //     "This NCycle has a <make> <model> at <parameter>." if make and model are arrays and the index of parameter exists in each.
+    //     "This NCycle was not created properly." if neither of the above are true.
+  }
+
+  // Create a new method printAll, which returns nothing and has no parameters.
+  printAll() {
+    // Use type guards and appropriate techniques to implement printAll such that it logs the same statements as print, but for all matching pairs in the make and model arrays, if applicable.
+  }
+}
